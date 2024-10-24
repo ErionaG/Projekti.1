53110295faa5114f4e4a9c32caa6efdc79549949
ErionaG <erionaagashiii@gmail.com>
2024-10-24 11:39:31 +0000

    upp respo

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-const menuIcon = document.getElementById('menu-icon');
+/*const menuIcon = document.getElementById('menu-icon');
 const menu = document.getElementById('menu');
 
 menuIcon.addEventListener('click', () => {
     menu.classList.toggle('show'); // Shto ose hiq klasën 'show' për menunë
-});
+});*/
+
