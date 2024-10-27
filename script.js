6b803c57bc3c1aee717ce8a188e84e74487e72dc
ErionaG <erionaagashiii@gmail.com>
2024-10-27 22:57:48 +0000

    upp

--- script.js
+++
@@ -1,7 +0,0 @@
-/*const menuIcon = document.getElementById('menu-icon');
-const menu = document.getElementById('menu');
-
-menuIcon.addEventListener('click', () => {
-    menu.classList.toggle('show'); // Shto ose hiq klasën 'show' për menunë
-});*/
-
